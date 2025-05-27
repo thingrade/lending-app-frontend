09a35199af8e2becd28de5b97771eacb319f1329
thingrade <thingrade@gmail.com>
2025-05-27 15:09:11 +0800

    Fix unit test condition in karma

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,10 +20,10 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('lending-app-frontend');
   });
 
-  it('should render title', () => {
+  it('should contain router outlet', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, lending-app-frontend');
+    expect(compiled.querySelector('router-outlet')).not.toBeNull();
   });
 });
